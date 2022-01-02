484a5948668ef331120d178127b51c55ca9bbf02
Nicholas Shephard <shephardn@gmail.com>
2022-01-02 10:33:20 -0600

    commit tree 830efbbfd5e68fe656829f846302a59238e08b7a

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,30 @@
+require 'pry'
 # Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+
+    attr_accessor :emails
+
+    def initialize(emails)
+        @emails = emails
+        # self.parse(emails)
+    end
+
+    def parse
+        # if self.emails =~ /,/
+        #     self.emails.split(", ").uniq
+        # else
+        #     self.emails.split(" ").uniq
+        # end
+        self.emails.split(/, | /).uniq
+
+    end
+end
+csv1 = "nick@gmail.com, ben@gmail.com, sally@yahoo.com"
+space1 = "nick@gmail.com ben@gmail.com sally@yahoo.com"
+
+
+# binding.pry
